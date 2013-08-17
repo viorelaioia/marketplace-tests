225e36c2037785c8dd2a8bf620f5802b20606910
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-08-17 12:32:56 -0700

    move settings out

--- cron.py
+++ cron.py
@@ -1,39 +1,17 @@
 import json
 import grequests
 import requests
+import sys
 
 from datetime import datetime
-
-JENKINS = [
-    # Payments Dev
-    'b2g.unagi.mozril.gaia.v1-train.ui.marketplace_payment',
-
-    # Marketplace Dev on B2G
-    'b2g.unagi.mozril.gaia.v1-train.ui.marketplace',
-
-    # Dev Jobs
-    'marketplace.dev',
-    'marketplace.dev.saucelabs',
-    'marketplace.dev.developer_hub',
-    'marketplace.dev.developer_hub.saucelabs',
-    'marketplace.dev.mobile.saucelabs',
-
-    # Prod Jobs
-    'marketplace.prod',
-    'marketplace.prod.saucelabs',
-    'marketplace.prod.mobile.saucelabs',
-
-    # Stage Jobs
-    'marketplace.stage.saucelabs',
-    'marketplace.stage.mobile.saucelabs',
-]
+from localsettings import *
 
 
 def get_jenkins(jobs):
     reqs = []
     for job in jobs:
-        url = ('http://qa-selenium.mv.mozilla.com:8080/job/{0}/'
-               'lastCompletedBuild/api/json'.format(job))
+        url = ('http://{0}/job/{1}/'
+               'lastCompletedBuild/api/json'.format(JENKINS_SERVER, job))
         reqs.append(grequests.get(url, headers={'Accept': 'application/json'}))
 
     resps = grequests.map(reqs)
@@ -49,7 +27,7 @@ def get_jenkins(jobs):
 
 
 if __name__ == '__main__':
-    result = json.dumps(get_jenkins(JENKINS), indent=2)
+    result = json.dumps(get_jenkins(JENKINS_JOBS), indent=2)
     status_file = open('status.json', 'w')
     status_file.write(result)
     status_file.close()
